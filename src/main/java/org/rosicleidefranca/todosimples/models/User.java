0c43d7e4b71e2551c52f32b871c34c2970cc9ae5
Rosicleide <r.l.f_franca@hotmail.com>
2025-11-26 16:06:43 -0300

    Criaçao do modelo User

---
+++ src/main/java/org/rosicleidefranca/todosimples/models/User.java
@@ -0,0 +1,46 @@
+package org.rosicleidefranca.todosimples.models;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name="users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
+public class User {
+    public interface CreateUser{}
+    public interface UpdateUser{}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @Size(groups=CreateUser.class,min = 2, max = 100)
+    @NotEmpty(groups=CreateUser.class)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "password",length = 60, nullable = false)
+    @NotNull (groups={CreateUser.class, UpdateUser.class})
+    @Size(groups= {CreateUser.class,UpdateUser.class},min = 2, max = 100)
+    @NotEmpty(groups={CreateUser.class,UpdateUser.class})
+    private String password;
+
+    //private List<Task> tasks = new ArrayList<task>();
+}
